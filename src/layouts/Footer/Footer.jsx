c11428f294696cb1291f6fa0e0854fab3d4c8663
Den Marshall <dank03052005@gmail.com>
2023-04-12 00:46:14 +0300

    fixed: deleted memo, because it was not needed at all

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -1,4 +1,3 @@
-import { memo } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 
 import './Footer.css';
@@ -8,58 +7,59 @@ import telegramIcon from './telegram.svg';
 import mailIcon from './mail.svg';
 import githubIcon from './github.svg';
 
-export const Footer = memo(() => {
+export const Footer = () => {
+  console.log('footer render');
   return (
     <>
       <Outlet />
-      <footer className="footer">
-        <Link to={'/greeting'} className="footer__help" href="#">
+      <footer className='footer'>
+        <Link to={'/greeting'} className='footer__help' href='#'>
           Help
         </Link>
-        <p className="footer__links">
+        <p className='footer__links'>
           Contact:
           <a
-            href="https://github.com/Den1Marshall"
-            target="_blank"
-            rel="noopener noreferrer"
+            href='https://github.com/Den1Marshall'
+            target='_blank'
+            rel='noopener noreferrer'
           >
-            <img className="footer__link-icon" src={githubIcon} alt="Github" />
+            <img className='footer__link-icon' src={githubIcon} alt='Github' />
           </a>
           <a
-            href="https://www.instagram.com/d.e.n_marshall/"
-            target="_blank"
-            rel="noopener noreferrer"
+            href='https://www.instagram.com/d.e.n_marshall/'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             <img
-              className="footer__link-icon"
+              className='footer__link-icon'
               src={instagramIcon}
-              alt="Instagram"
+              alt='Instagram'
             />
           </a>
           <a
-            href="https://t.me/den_marshall"
-            target="_blank"
-            rel="noopener noreferrer"
+            href='https://t.me/den_marshall'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             <img
-              className="footer__link-icon"
+              className='footer__link-icon'
               src={telegramIcon}
-              alt="Telegram"
+              alt='Telegram'
             />
           </a>
           <a
-            href="mailto:denyshrychulevych@gmail.com"
-            target="_blank"
-            rel="noopener noreferrer"
+            href='mailto:denyshrychulevych@gmail.com'
+            target='_blank'
+            rel='noopener noreferrer'
           >
-            <img className="footer__link-icon" src={mailIcon} alt="Mail" />
+            <img className='footer__link-icon' src={mailIcon} alt='Mail' />
           </a>
         </p>
-        <p className="footer__copyright">
+        <p className='footer__copyright'>
           &copy; Denys Hrychulevych, 2022-{new Date().getFullYear()}. All rights
           reserved.
         </p>
       </footer>
     </>
   );
-});
+};
