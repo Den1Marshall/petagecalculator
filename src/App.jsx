378942317fc1700d889817476a66ac39fd48a287
Den Marshall <dank03052005@gmail.com>
2023-04-12 00:40:44 +0300

    added: AnimalProvider context to App.jsx, including reducer logic. fixed: greeting screen first show logic migrated to Home.jsx, so that no longer causing all-time re-render of the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,29 @@
-import { useEffect } from 'react';
-import { Route, Routes, useNavigate } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+
+import { ThemeProvider } from '@emotion/react';
+import { Theme } from './utils/Theme';
+
+import AnimalProvider from './context/AnimalContext';
 
 import { Footer } from './layouts/Footer/Footer';
 import { Greeting } from './pages/Greeting/Greeting';
 import { Home } from './pages/Home/Home';
 import { Animals } from './pages/Animals/Animals';
 
-import { ThemeProvider } from '@emotion/react';
-import { Theme } from './utils/Theme';
-
 export const App = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (localStorage.getItem('greetingVisible') === null) {
-      navigate('/greeting');
-    }
-  }, [navigate]);
+  console.log('app render');
 
   return (
     <ThemeProvider theme={Theme}>
-      <Routes>
-        <Route path='/' element={<Footer />}>
-          <Route index element={<Home />} />
-          <Route path='/animals' element={<Animals />} />
-          <Route path='/greeting' element={<Greeting />} />
-        </Route>
-      </Routes>
+      <AnimalProvider>
+        <Routes>
+          <Route path='/' element={<Footer />}>
+            <Route index element={<Home />} />
+            <Route path='/animals' element={<Animals />} />
+            <Route path='/greeting' element={<Greeting />} />
+          </Route>
+        </Routes>
+      </AnimalProvider>
     </ThemeProvider>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,21 @@
 import './Home.css';
 
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { HomeTitle } from '../../components/HomeTitle/HomeTitle';
 import { HomeCalculator } from '../../components/HomeCalculator/HomeCalculator';
 
 export const Home = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (localStorage.getItem('greetingVisible') === null) {
+      navigate('/greeting');
+    }
+  }, [navigate]);
+
+  console.log('home render');
+
   return (
     <main className='home'>
       <div className='container'>
