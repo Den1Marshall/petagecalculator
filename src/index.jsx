3cc6900e1a06b58545cf15a2fb512a9a5acb4a56
Den Marshall <dank03052005@gmail.com>
2023-04-14 17:30:39 +0300

    fixed: properly added normalize.css, namely moved import from index.jsx to index.css

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-// libs
-import 'normalize.css';
-
 // global styles
 import './index.css';
 
