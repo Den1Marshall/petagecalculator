5532acb5d4552d185917de5569acd505b8530159
Denys Hrychulevych <denyshrychulevych@gmail.com>
2024-04-21 18:14:20 +0300

    feat: add homepage title

--- src/views/Home/ui/Home.tsx
+++ src/views/Home/ui/Home.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
     <main className='font-pacifico'>
-      <h1 className='text-6xl text-center'>Hello, World!</h1>
+      <h1 className='mt-4 text-8xl text-center'>
+        Pet Age <br />
+        <span className='text-5xl'>calculator</span>
+      </h1>
     </main>
   );
 }
