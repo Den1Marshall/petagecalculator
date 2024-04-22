e30b210eae3e7fb0b4e529b48ecf0996e9408abf
Denys Hrychulevych <denyshrychulevych@gmail.com>
2024-04-22 22:02:53 +0300

    feat: create AnimalButton entity

---
+++ src/entities/AnimalButton/index.ts
@@ -0,0 +1,1 @@
+export { AnimalButton } from './ui/AnimalButton';
---
+++ src/entities/AnimalButton/ui/AnimalButton.tsx
@@ -0,0 +1,41 @@
+'use client';
+import { MotionButton } from '@/shared/ui/MotionButton';
+import { useAnimationControls } from 'framer-motion';
+import { StaticImport } from 'next/dist/shared/lib/get-img-props';
+import Image from 'next/image';
+import { FC } from 'react';
+
+interface AnimalButtonProps {
+  src: string | StaticImport;
+  name: string;
+  isHuman?: boolean;
+  className?: string;
+  openModal?: () => void;
+}
+
+export const AnimalButton: FC<AnimalButtonProps> = ({
+  src,
+  name,
+  isHuman,
+  openModal,
+  className,
+}) => {
+  const controls = useAnimationControls();
+
+  return (
+    <MotionButton
+      onPress={openModal}
+      onPressStart={() => controls.start({ x: isHuman ? [10, -10, 0] : 0 })}
+      animate={controls}
+      whileTap={{ scale: isHuman ? 1 : 0.85 }}
+      transition={{
+        type: 'spring',
+        duration: isHuman ? 0.5 : 0.3,
+        bounce: isHuman ? 0.9 : 0,
+      }}
+      className={`bg-white rounded-full max-w-[200px] h-auto focus:outline-none focus-visible:outline-[revert] ${className}`}
+    >
+      <Image quality={100} priority src={src} alt={`${name} icon`} />
+    </MotionButton>
+  );
+};
