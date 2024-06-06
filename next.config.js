7e393325e14766de7203c649d99d01b790383caa
Denys Hrychulevych <denyshrychulevych@gmail.com>
2024-06-06 16:42:50 +0300

    chore: disable pwa support in development mode

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {};
 
 const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
 });
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
