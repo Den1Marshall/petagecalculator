f72b8c9ad3a296fc36118bf1be6b4919194a78d1
Denys Hrychulevych <denyshrychulevych@gmail.com>
2024-06-23 02:27:36 +0300

    fix: homepage title margin

--- src/views/Home/ui/Home.tsx
+++ src/views/Home/ui/Home.tsx
@@ -4,7 +4,7 @@ import { Calculator } from '@/widgets/Calculator';
 export default function Home() {
   return (
     <main className='h-full flex flex-col items-center max-lg:landscape:gap-20 font-pacifico'>
-      <h1 className='mt-4 text-8xl text-center'>
+      <h1 className='text-8xl text-center'>
         Pet Age <br />
         <span className='text-5xl'>calculator</span>
       </h1>
