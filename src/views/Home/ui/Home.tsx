33bae90ea48cf12ede4d73c1736c950ca1ad3efe
Denys Hrychulevych <denyshrychulevych@gmail.com>
2024-06-28 02:04:16 +0300

    fix: homepage height does not include bottom nav height

--- src/views/Home/ui/Home.tsx
+++ src/views/Home/ui/Home.tsx
@@ -3,7 +3,7 @@ import { Calculator } from '@/widgets/Calculator';
 
 export default function Home() {
   return (
-    <main className='h-full flex flex-col items-center max-lg:landscape:gap-20 font-pacifico'>
+    <main className='h-[calc(100%_-_64px)] flex flex-col items-center max-lg:landscape:gap-20 font-pacifico'>
       <h1 className='text-8xl text-center'>
         Pet Age <br />
         <span className='text-5xl'>calculator</span>
