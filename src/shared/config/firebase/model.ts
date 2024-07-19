55108bbf89d1044bbafff045d08749452e85650f
Denys Hrychulevych <denyshrychulevych@gmail.com>
2024-07-20 00:04:52 +0300

    chore: move firebase config values to .local.env

--- src/shared/config/firebase/model.ts
+++ src/shared/config/firebase/model.ts
@@ -3,13 +3,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA0ljjE_PyGC96brNa513EOdkgYLRFmkcY',
-  authDomain: 'petagecalculator.firebaseapp.com',
-  projectId: 'petagecalculator',
-  storageBucket: 'petagecalculator.appspot.com',
-  messagingSenderId: '897342599798',
-  appId: '1:897342599798:web:f1ba0e42b9280da2756557',
-  measurementId: 'G-4F15FWY0PK',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
