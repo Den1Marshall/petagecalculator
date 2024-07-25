f217c070e1a766af7e4f6098533aaeeeed28532a
Denys Hrychulevych <denyshrychulevych@gmail.com>
2024-07-25 16:39:38 +0300

    feat(pwa): precache all pages, links and scripts for offline usage

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const nextConfig = {};
 const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
 });
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
