08c74c61573e4263d41e25af23ad6083417da522
Denys Hrychulevych <denyshrychulevych@gmail.com>
2024-08-24 01:12:28 +0300

    feat(reauthenticateUser)!: remove empty password & email props case (prompt input)

--- src/shared/api/reauthenticateUser.ts
+++ src/shared/api/reauthenticateUser.ts
@@ -8,8 +8,8 @@ import {
 
 export const reauthenticateUser = async (
   user: User,
-  email?: string,
-  password?: string
+  email: string,
+  password: string
 ): Promise<void> => {
   try {
     switch (user.providerData[0].providerId) {
@@ -18,25 +18,11 @@ export const reauthenticateUser = async (
         break;
 
       case 'password':
-        if (email !== undefined && password !== undefined) {
-          await reauthenticateWithCredential(
-            user,
-            EmailAuthProvider.credential(email, password)
-          );
-          break;
-        } else {
-          email = prompt('Enter your email') ?? undefined;
-          if (email === undefined) throw new Error('Empty email');
-
-          password = prompt('Enter your password') ?? undefined;
-          if (password === undefined) throw new Error('Empty password');
-
-          await reauthenticateWithCredential(
-            user,
-            EmailAuthProvider.credential(email, password)
-          );
-          break;
-        }
+        await reauthenticateWithCredential(
+          user,
+          EmailAuthProvider.credential(email, password)
+        );
+        break;
 
       default:
         break;
