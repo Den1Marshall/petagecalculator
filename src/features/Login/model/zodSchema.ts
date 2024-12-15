e8202f81442fc111077c3835ab5445ec1ccfdcda
Denys Hrychulevych <111864961+Den1Marshall@users.noreply.github.com>
2024-12-15 15:53:44 +0200

    fix(Login): add 'required_error' for zodSchema

--- src/features/Login/model/zodSchema.ts
+++ src/features/Login/model/zodSchema.ts
@@ -1,22 +1,31 @@
 import { z } from 'zod';
 
 export const registerSchema = z.object({
-  email: z.string().email().min(1, { message: 'Email is required' }),
+  email: z
+    .string({ required_error: 'Email is required' })
+    .min(1, { message: 'Email is required' })
+    .email(),
   password: z
-    .string()
+    .string({ required_error: 'Password is required' })
     .min(1, { message: 'Password is required' })
     .min(6, { message: 'Must be 6 or more characters long' })
     .max(4096, { message: 'Must be 4096 or fewer characters long' }),
 });
 
 export const loginSchema = z.object({
-  email: z.string().email().min(1, { message: 'Email is required' }),
+  email: z
+    .string({ required_error: 'Email is required' })
+    .min(1, { message: 'Email is required' })
+    .email(),
   password: z
-    .string()
+    .string({ required_error: 'Password is required' })
     .min(1, { message: 'Password is required' })
     .max(4096, { message: 'Must be 4096 or fewer characters long' }),
 });
 
 export const loginForgotPasswordSchema = z.object({
-  email: z.string().email().min(1, { message: 'Email is required' }),
+  email: z
+    .string({ required_error: 'Email is required' })
+    .min(1, { message: 'Email is required' })
+    .email(),
 });
